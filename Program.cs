c1000529da417b8fd35188be839c034dd58eecb6
danielaniceto <danielgustavots@hotmail.com>
2022-06-13 16:28:20 -0300

    First Commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+int numeroDeVezes = 10;
+
+for (int i=0; i < numeroDeVezes; i++)
+{
+    Console.WriteLine($"Daniel {i}");
+}
+    
+
